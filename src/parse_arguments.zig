3fb72d8c77ee464f85ad03294e58b7c8beb388df
evan <mevanlauderdaleyt@gmail.com>
2025-03-17 18:52:02 -0500

    In parse_arguments, help will now return a ParsedCommand union result

--- src/parse_arguments.zig
+++ src/parse_arguments.zig
@@ -8,18 +8,19 @@ const ParseError = error{
 };
 
 const ParsedCommand = union {
+    help: bool,
     input_filename: []u8,
     ComplexCommand: struct {
         input_filename: []u8,
         output_filename: []u8,
-        scale: u8,
-        color: bool,
+        scale: u8 = 1,
+        color: bool = false,
     },
 };
 
 // maybe instead of void we return an enum/some object
 // that can capture valid command info and execute on it.
-pub fn parse_arguments(allocator: std.mem.Allocator) ParseError!void {
+pub fn parse_arguments(allocator: std.mem.Allocator) ParseError!ParsedCommand {
     const arguments = std.process.argsAlloc(allocator) catch |err| {
         std.debug.print("{}", .{err});
         return ParseError.AllocationFailure;
@@ -27,11 +28,9 @@ pub fn parse_arguments(allocator: std.mem.Allocator) ParseError!void {
     defer std.process.argsFree(allocator, arguments);
 
     if (arguments.len == 1 or std.mem.eql(u8, "help", arguments[1])) {
-        // check if there are more arguments are this
         if (arguments.len > 2) return ParseError.InvalidSubcommand;
 
-        std.debug.print("HELP\n", .{});
-        //instead of commands.help(); mayybe we return a command obj to execute on?
-        return;
+        const parsed_command = ParsedCommand{ .help = true };
+        return parsed_command;
     }
 }
