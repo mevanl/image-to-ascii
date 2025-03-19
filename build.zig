13b44a9835c788c1df9d363a723f40bf25e1c1e5
evan <mevanlauderdaleyt@gmail.com>
2025-03-19 17:56:06 -0500

    Modified build.zig to properly add stb_image library as a static llibrary

--- build.zig
+++ build.zig
@@ -1,13 +1,29 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Setup stb_image as library
+    const stb_image_library = b.addStaticLibrary(.{
+        .name = "stb_image",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    stb_image_library.addCSourceFile(.{ .file = b.path("src/lib/stb_wrapper.c") });
+    stb_image_library.linkLibC();
+    stb_image_library.installHeader(b.path("src/lib/stb_image.h"), "src/lib/stb_image.h");
+    b.installArtifact(stb_image_library);
+
+    // define our executable
     const exe = b.addExecutable(.{
         .name = "image-to-ascii",
         .root_source_file = b.path("src/main.zig"),
-        .target = b.graph.host,
+        .target = target,
+        .optimize = optimize,
     });
-    exe.linkLibC();
-    exe.addCSourceFiles(&[_][]const u8{"src/lib/stb_wrapper.c"}, &[_][]const u8{ "-g", "-O3" });
+    exe.linkLibrary(stb_image_library);
 
     b.installArtifact(exe);
 }
